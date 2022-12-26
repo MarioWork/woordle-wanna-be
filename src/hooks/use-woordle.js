da299b8b1b138ac97d277635c91e16394ff7fd8d
MarioWork <mariovieira.work@hotmail.com>
2022-12-26 19:51:01 +0000

    addGuess function and removeLastGuess function

--- src/hooks/use-woordle.js
+++ src/hooks/use-woordle.js
@@ -6,15 +6,31 @@ const useWoordle = () => {
     const [currentGuess, setCurrentGuess] = useState([]);
 
     const addGuess = (guess) => {
-        if (currentGuess.length < NUMBER_OF_LETTERS)
+        if (currentGuess.length < NUMBER_OF_LETTERS) {
             setCurrentGuess((currentGuess) => [...currentGuess, guess]);
+            return;
+        }
+
+        if (currentGuess.length === NUMBER_OF_LETTERS) {
+            setCurrentGuess((currentGuess) => {
+                const currentGuessCopy = currentGuess;
+                currentGuessCopy[NUMBER_OF_LETTERS - 1] = guess;
+                return [...currentGuessCopy];
+            });
+            return;
+        }
+    };
+
+    const removeLastGuess = () => {
+        console.log("clean");
     };
 
     return {
         currentGuess,
-        addGuess
-    }
-}
+        addGuess,
+        removeLastGuess,
+    };
+};
 
 
 export default useWoordle;
