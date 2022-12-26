c2ea022bcc50e9f3dd9db8d36e85a129867b5486
MarioWork <mariovieira.work@hotmail.com>
2022-12-26 17:31:33 +0000

    add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import PropTypes from "prop-types";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+App.propTypes = {};
 
 export default App;
