dc501765ab95c678ddf5896181547a8bddcf1264
MarioWork <mariovieira.work@hotmail.com>
2022-12-26 18:00:54 +0000

    create first version of keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { keys } from "./constants/keys";
+import styled from "styled-components";
+styled;
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <StyledContent>
+      <StyledKeyboard>
+        {keys.map((row) => (
+          <StyledKeyboardRow>
+            {row.map((key) => (
+              <StyledKey>{key}</StyledKey>
+            ))}
+          </StyledKeyboardRow>
+        ))}
+      </StyledKeyboard>
+    </StyledContent>
+  );
 };
 
-App.propTypes = {};
-
 export default App;
+
+const StyledContent = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const StyledKeyboard = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const StyledKeyboardRow = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+`;
+
+const StyledKey = styled.button`
+  background: blue;
+  border-radius: 25px;
+  padding: 1em;
+`;
