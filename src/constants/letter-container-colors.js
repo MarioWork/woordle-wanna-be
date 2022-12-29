fb42d981937e6f0faaea0337dbef8b6ef61e6d9e
MarioWork <mariovieira.work@hotmail.com>
2022-12-29 13:18:47 +0000

    create new enum for colors

---
+++ src/constants/letter-container-colors.js
@@ -0,0 +1,8 @@
+import { LetterContainerType } from "./letter-container-type";
+
+
+export const LetterContainerColors = {
+    [LetterContainerType.WRONG_SPOT]: "yellow",
+    [LetterContainerType.NON_EXISTENT]: "black",
+    [LetterContainerType.RIGHT_SPOT]: "green",
+};
