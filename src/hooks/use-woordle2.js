d292e48282e70c94bc7f25716aaa23668bef8968
MarioWork <mariovieira.work@hotmail.com>
2023-01-02 17:29:33 -0100

    added first version of add/remove/submit functions

--- src/hooks/use-woordle2.js
+++ src/hooks/use-woordle2.js
@@ -1,6 +1,8 @@
 import { useReducer } from "react";
 import { LetterContainerType } from "../constants/letter-container-type";
 
+const NUMBER_OF_LETTERS = 5;
+const NUMBER_OF_TRIES = 6;
 
 const createDefaultGuessHistory = () => {
     let history = [];
@@ -26,19 +28,92 @@ const initialState = {
     guessHistory: createDefaultGuessHistory(),
 }
 
+const Actions = {
+    ADD_LETTER: "add-letter",
+    REMOVE_LETTER: "remove-letter",
+    SUBMIT_GUESS: "submit-guess"
+}
+
 const useWoordle2 = () => {
 
+    const addLetter = (state, letter) => {
+        const { currentGuess } = state;
+        const upperCaseLetter = letter.toUpperCase();
+
+        //Alternate last letter to the new one
+        if (currentGuess.length >= NUMBER_OF_LETTERS) {
+            const currentGuessCopy = currentGuess;
+
+            currentGuessCopy[NUMBER_OF_LETTERS - 1] = {
+                letter: upperCaseLetter,
+                containerType: LetterContainerType.DEFAULT
+            };
+
+            return { ...state, currentGuess: currentGuessCopy };
+        }
+
+        return {
+            ...state,
+            currentGuess:
+                [...currentGuess,
+                {
+                    letter: upperCaseLetter,
+                    containerType: LetterContainerType.DEFAULT
+                }
+                ]
+        }
+    }
+
+    const removeLastLetterOfCurrentGuess = (state) => {
+        const { currentGuess } = state;
+        return {
+            ...state,
+            currentGuess:
+                [
+                    ...currentGuess.slice(0, currentGuess.length - 1)
+                ]
+        };
+    }
+
+    const submitGuess = (state) => {
+        const { currentGuess, guessCount } = state;
+
+        if (currentGuess.length !== NUMBER_OF_LETTERS) return state;
+
+        //verify guess
+
+        return {
+            ...state,
+            guessCount: guessCount + 1,
+            currentGuess: []
+        };
+    }
 
     const reducer = (state, { action, payload }) => {
-
+        switch (action) {
+            case Actions.ADD_LETTER:
+                return addLetter(state, payload);
+            case Actions.REMOVE_LETTER:
+                return removeLastLetterOfCurrentGuess(state);
+            case Actions.SUBMIT_GUESS:
+                return submitGuess(state);
+        }
     }
 
 
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [{ guessHistory, hasWon, isGameOver }, dispatch] = useReducer(reducer, initialState);
 
     return {
-        x: 0
+        guessHistory,
+        hasWon,
+        isGameOver,
+        addLetterToCurrentGuess: (letter) =>
+            dispatch({ action: Actions.ADD_LETTER, payload: letter }),
+        removeLastLetterOfCurrentGuess: () =>
+            dispatch({ action: Actions.REMOVE_LETTER }),
+        submitGuess: () =>
+            dispatch({ action: Actions.SUBMIT_GUESS }),
     }
 };
 
