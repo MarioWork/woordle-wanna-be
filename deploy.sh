bfb4bdc1996516d9a2b68259bd370c2f9c27e6ba
MarioWork <mariovieira.work@hotmail.com>
2023-01-03 20:21:03 -0100

    fixes for deploy

--- deploy.sh
+++ deploy.sh
@@ -21,12 +21,12 @@ echo > .nojekyll
 git init
 git checkout -B main
 git add -A
-git commit -m "deploy - `date`"
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mariowork/woordle-wanna-be main:gh-pages
+git push -f git@github.com:MarioWork/woordle-wanna-be.git main:gh-pages
 
 cd -
