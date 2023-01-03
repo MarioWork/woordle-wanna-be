b825f80c9f27a453608eb7ab59668e9312826073
MarioWork <mariovieira.work@hotmail.com>
2023-01-03 20:14:34 -0100

    add better color for debug

--- src/constants/letter-container-colors.js
+++ src/constants/letter-container-colors.js
@@ -3,7 +3,7 @@ import { LetterContainerType } from "./letter-container-type";
 
 export const LetterContainerColors = {
     [LetterContainerType.WRONG_SPOT]: "yellow",
-    [LetterContainerType.NON_EXISTENT]: "black",
+    [LetterContainerType.NON_EXISTENT]: "#929292",
     [LetterContainerType.RIGHT_SPOT]: "green",
     [LetterContainerType.DEFAULT]: "#cbcbcb",
 };
