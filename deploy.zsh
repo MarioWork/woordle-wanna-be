ca8a39747fa55ea660b678fabe1db8e5cb46cb3b
MarioWork <mariovieira.work@hotmail.com>
2023-01-03 20:07:59 -0100

    add deploy script

---
+++ deploy.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# If it is a redeploy uncomment the line bellow
+#rm -rf dist
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m "deploy - `date`"
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mariowork/woordle-wanna-be main:gh-pages
+
+cd -
